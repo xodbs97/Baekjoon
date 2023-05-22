37f79db5cd7027d48f8757bcf4e35b54da2a19a1
Taeyoon Kim <92427843+TAEY00N@users.noreply.github.com>
2023-05-22 10:34:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	std::cout << "         ,r\'\"7\nr`-_   ,\'  ,\/\n \\. \". L_r\'\n   `~\\\/\n      |\n      |";
+
+	return 0;
+}
