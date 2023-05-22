ae6e2d881f0b6822efd9f127f4dc1a3a61010cd8
Taeyoon Kim <92427843+TAEY00N@users.noreply.github.com>
2023-05-22 12:25:16 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+int main(int argc, const char* argv[])
+{
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	std::string str;
+	std::cin >> str;
+
+	std::cout << str.length();
+
+	return 0;
+}
