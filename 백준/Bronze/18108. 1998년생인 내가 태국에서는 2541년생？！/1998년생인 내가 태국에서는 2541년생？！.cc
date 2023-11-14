87f6616acce3ed3ca4e45955780447c8b59b822c
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-11-14 17:08:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int b = 2541 - 1998;
+    a -= b;
+    
+    cout << a;
+
+    return 0; // You should include a return statement
+}
