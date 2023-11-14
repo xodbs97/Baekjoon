f9bf6eaece88ff964f0c165f251eca63b9cb0d3b
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-11-14 17:05:35 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+
+    a += "??!";
+    
+    cout << a;
+
+    return 0; // You should include a return statement
+}
