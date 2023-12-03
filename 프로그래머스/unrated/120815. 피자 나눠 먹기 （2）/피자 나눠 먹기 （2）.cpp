8608792c3a849e40ffc5f3c38cdf539c55a4373f
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-12-04 01:18:18 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while(answer * 6 % n != 0){
+        answer++;
+    }
+    return answer;
+}
