c0375a2d79c70ef1b76a4524f8fc40a2affec386
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-12-05 00:22:38 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = -1000000000;
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = i + 1; j < numbers.size(); j++){
+            if(numbers[i] * numbers[j] > answer){
+                answer = numbers[i] * numbers[j];
+            }
+        }
+    }
+    return answer;
+}
