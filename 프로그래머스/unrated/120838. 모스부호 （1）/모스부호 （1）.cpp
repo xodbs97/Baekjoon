affa58ee3576c3cd77d441b88655f3bb1ea71de4
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-12-06 22:56:15 +0900

    [level unrated] Title: 모스부호 (1), Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.cpp
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -9,9 +9,7 @@ string solution(string letter) {
     vector<string> m = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
     
 	while (letter.find(" ") != string::npos) {
-		string morse = letter.substr(0, letter.find(" "));
-		auto idx = find(m.begin(), m.end(), morse) - m.begin();
-		answer += 'a' + static_cast<char>(idx);
+		answer += 'a' + static_cast<char>(find(m.begin(), m.end(), letter.substr(0, letter.find(" "))) - m.begin());
 		letter.erase(0, letter.find(" ") + 1);
 	}
 	answer += 'a' + static_cast<char>(find(m.begin(), m.end(), letter) - m.begin());
