d9a934d49b828eead10f678249feb4cd0dcf6241
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2023-12-12 01:42:09 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_str, int n) {
+    vector<string> answer;
+    while(my_str != ""){
+        if(my_str.length() > n){
+            answer.push_back(my_str.substr(0, n));
+            my_str.erase(0, n);
+        }else{
+            answer.push_back(my_str);
+            my_str = "";
+        }
+    }
+    return answer;
+}
