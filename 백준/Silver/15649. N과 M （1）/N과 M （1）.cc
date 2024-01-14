57728190825c1410d91132e2fb58da0f0853f57a
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-01-14 15:30:05 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+bool is_used[10];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!is_used[i]) {
+			arr[k] = i;
+			is_used[i] = true;
+			func(k + 1);
+			is_used[i] = false;
+		}
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	func(0);
+
+	return 0;
+}
