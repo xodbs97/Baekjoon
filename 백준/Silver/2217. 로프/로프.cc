91a7926b0b3b63da828d3e80098328a34c77a13b
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-01-23 02:05:45 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int rope[100005];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> rope[i];
+	sort(rope, rope + n);
+
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		ans = max(ans, rope[n - i] * i);
+	}
+	cout << ans;
+
+	return 0;
+}
