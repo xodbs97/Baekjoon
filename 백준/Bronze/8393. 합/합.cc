56bfb3a5d8110a6c94ab3a9957f52e2052088955
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-02-01 18:32:26 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	cout << (n * n + n) / 2;
+    
+	return 0;
+}
