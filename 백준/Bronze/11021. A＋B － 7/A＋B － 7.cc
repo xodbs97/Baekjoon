04e0f1ad6dcf33aeced7af06d085daa75a093584
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-02-04 13:02:21 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+
+	return 0;
+}
