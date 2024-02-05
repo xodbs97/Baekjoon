428169dfafc890aee38cd62118e6aec1a0cbbc8f
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-02-05 22:46:39 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	cout << 1 - n << " ";
+	cin >> n;
+	cout << 1 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 8 - n << " ";
+
+	return 0;
+}
