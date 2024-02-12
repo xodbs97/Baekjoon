34769abe4306b59f8bee2a46cfe796fc801384ea
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-02-12 16:23:22 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 자연수 개수, 수열 길이
+int n, m;
+// 완성된 수열이 저장될 배열
+int arr[10];
+
+// 현재 예비 번호, 현재 수열 길이
+void bt(int num, int k) {
+	// 수열이 완성됐다면
+	if (k == m) {
+		for (int i = 0; i < m; ++i) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = num; i <= n; ++i) {
+		arr[k] = i;
+		bt(i, k + 1);
+	}
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n >> m;
+	bt(1, 0);
+
+	return 0;
+}
