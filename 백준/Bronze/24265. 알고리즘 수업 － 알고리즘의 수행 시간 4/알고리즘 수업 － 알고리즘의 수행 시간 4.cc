c96e74a6207f9fcf702b9f706ad91647e6059e3f
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-02-27 01:08:16 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll n;
+    cin >> n; 
+    cout<< n * (n - 1) / 2 << "\n" << 2;
+    
+    return 0;
+}
