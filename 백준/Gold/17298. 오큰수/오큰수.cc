1ec36312ce97279b386a3e0313d865714966272a
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-07-24 23:11:27 +0900

    [Gold IV] Title: 오큰수, Time: 220 ms, Memory: 10212 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,34 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <vector>
+
 using namespace std;
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-	int n; // 수열 크기
+	int n;
 	cin >> n;
 
-	stack<int> st; // nge가 필요한 수들을 저장하기 위한 공간
-	vector<int> nge(n, 0); // 각각의 수열의 nge 값을 저장하기 위한 공간
-	for (int i = 0; i < n; i++) {
-		int a;
+	int i;
+	int a;
+	stack<int> left;
+	for (i = 1; i < n; ++i)
+	{
 		cin >> a;
-		if (st.size()) { // stack이 차있는 경우, 즉 nge를 기다리는 수가 1개 이상인 경우
-			if (st.top() < a) { // nge가 나타났을 때
-				int idx = i - 1;
-				while (st.size() && st.top() < a) { // 모든 수가 nge를 가지거나 스택에서 더 큰 수를 만나기 전까지
-					if (nge[idx] == -1) { // nge를 찾은 경우
-						nge[idx] = a;
-						st.pop();
-					}
-					--idx;
-				}
-			}
-		}
-		st.push(a);
-		nge[i] = -1;
+		left.push(a);
 	}
-	for (int i : nge) cout << i << " ";
+
+	stack<int> right;
+	cin >> a;
+	right.push(a);
+
+	vector<int> nge(n);
+	nge[n - 1] = -1;
+
+	for (i = n - 2; 0 <= i; --i)
+	{
+		int cur = left.top();
+		left.pop();
+
+		while (!right.empty() && right.top() <= cur)
+		{
+			right.pop();
+		}
+
+		if (right.empty())
+		{
+			nge[i] = -1;
+		}
+		else
+		{
+			nge[i] = right.top();
+		}
+		right.push(cur);
+	}
+
+	for (auto& num : nge)
+	{
+		cout << num << ' ';
+	}
 
 	return 0;
 }
