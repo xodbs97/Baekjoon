1da59ab26936bcdacad18a122810ecce4bf14e3c
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-08-06 18:16:14 +0900

    [Silver I] Title: 그림, Time: 16 ms, Memory: 6988 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,49 +1,95 @@
 #include <iostream>
-#include <queue>
 
-using namespace std;
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-#define X first
-#define Y second
+	int paint_num = 0;
+	int paint_size;
+	int largest_paint = 0;
 
-int board[502][502];
-bool vis[502][502];
-int n, m;
-int dx[4]{ 1,0,-1,0 };
-int dy[4]{ 0,1,0,-1 };
+	int i;
+	int j;
+	int k;
+	int n; // 도화지의 세로 크기
+	int m; // 도화지의 가로 크기
+	std::cin >> n >> m;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	const int X[4] = { 0,0,1,-1 };
+	const int Y[4] = { 1,-1,0,0 };
 
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) cin >> board[i][j];
+	int** board = new int* [n];
+	for (i = 0; i < n; ++i)
+	{
+		board[i] = new int[m];
+		for (j = 0; j < m; ++j)
+		{
+			std::cin >> board[i][j];
+		}
+	}
 
-	queue<pair<int, int>> q;
-	int pic_num = 0, pic_size = 0, max = 0;
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
-		if (board[i][j] == 1 && vis[i][j] == 0) {
-			++pic_num;
-			pic_size = 0;
-			q.push({ i,j });
-			vis[i][j] = true;
-			while (q.size()) {
-				pair<int, int> p = q.front();
-				++pic_size;
-				q.pop();
-				for (int k = 0; k < 4; k++) {
-					int nx = p.X + dx[k];
-					int my = p.Y + dy[k];
-					if (nx < 0 || nx >= n || my < 0 || my >= m) continue;
-					if (board[nx][my] != 1 || vis[nx][my]) continue;
-					q.push({ nx,my });
-					vis[nx][my] = true;
+	int* row_stk = new int[n * m * 2];
+	int* col_stk = new int[n * m * 2];
+	int size = 0;
+
+	int cur_row;
+	int cur_col;
+	int sub_row;
+	int sub_col;
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < m; ++j)
+		{
+			if (board[i][j])
+			{
+				++paint_num;
+
+				paint_size = 1;
+				row_stk[size] = i;
+				col_stk[size] = j;
+				++size;
+				board[i][j] = 0;
+
+				while (size)
+				{
+					cur_row = row_stk[size - 1];
+					cur_col = col_stk[size - 1];
+					--size;
+
+					for (k = 0; k < 4; ++k)
+					{
+						sub_row = cur_row + X[k];
+						sub_col = cur_col + Y[k];
+
+						if (sub_row < 0 || n <= sub_row || sub_col < 0 || m <= sub_col || board[sub_row][sub_col] == 0)
+						{
+							continue;
+						}
+
+						++paint_size;
+						row_stk[size] = sub_row;
+						col_stk[size] = sub_col;
+						++size;
+						board[sub_row][sub_col] = 0;
+					}
 				}
+
+				largest_paint = std::max(largest_paint, paint_size);
 			}
 		}
-		if (pic_size > max) max = pic_size;
 	}
-	cout << pic_num << "\n" << max;
+
+	std::cout << paint_num << '\n' << largest_paint;
+
+	for (i = 0; i < n; ++i)
+	{
+		delete[] board[i];
+	}
+	delete[] board;
+
+	delete[] row_stk;
+	delete[] col_stk;
 
 	return 0;
 }
