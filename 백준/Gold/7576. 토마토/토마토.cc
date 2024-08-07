0229e6d3cb508716c753a72c21b31d932960a260
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-08-07 22:48:42 +0900

    [Gold V] Title: 토마토, Time: 84 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,59 +1,98 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#include <queue>
 
 using namespace std;
 
-#define X first
-#define Y second
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	const int X[4] = { 1,-1,0,0 };
+	const int Y[4] = { 0,0,1,-1 };
 
-	int n, m;
-	cin >> m >> n;
+	int i;
+	int j;
+	int m; // 상자의 가로 칸의 수
+	int n; // 상자의 세로 칸의 수
+	cin >> m >> n; // 2 ≤ m,n ≤ 1,000
+
+	queue<pair<int, int>> tomato;
 
 	int** box = new int* [n];
 	int** days = new int* [n];
-	for (int i = 0; i < n; i++) {
+	for (i = 0; i < n; ++i)
+	{
 		box[i] = new int[m];
 		days[i] = new int[m];
-		for (int j = 0; j < m; j++) days[i][j] = 0;
-	}
-	int dx[4]{ 1,0,-1,0 }, dy[4]{ 0,1,0,-1 };
-
-	queue<pair<int, int>> tomato;
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
-		cin >> box[i][j];
-		if (box[i][j] == 0) days[i][j] = -1;
-		if (box[i][j] == 1) tomato.push({ i,j });
-	}
-
-	while (!tomato.empty()) {
-		auto cur = tomato.front(); tomato.pop();
-		for (int dir = 0; dir < 4; dir++) {
-			int px = cur.X + dx[dir], py = cur.Y + dy[dir];
-			if (px < 0 || px >= n || py < 0 || py >= m) continue;
-			if (days[px][py] >= 0) continue;
-			days[px][py] = days[cur.X][cur.Y] + 1;
-			tomato.push({ px,py });
+		for (j = 0; j < m; ++j)
+		{
+			cin >> box[i][j];
+			if (box[i][j] == 1)
+			{
+				tomato.push({ i,j });
+				days[i][j] = 0;
+			}
+			else
+			{
+				days[i][j] = -1;
+			}
 		}
 	}
 
-	int ans = 0;
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
-		if (days[i][j] == -1) {
-			cout << -1;
-			return 0;
-		}
-		ans = max(ans, days[i][j]);
-	}
-	cout << ans;
+	pair<int, int> cur_pos;
+	pair<int, int> sub_pos;
+	while (tomato.size())
+	{
+		cur_pos = tomato.front();
+		tomato.pop();
 
-	for (int i = 0; i < n; i++) {
+		for (i = 0; i < 4; ++i)
+		{
+			sub_pos = { cur_pos.first + X[i],cur_pos.second + Y[i] };
+			if (sub_pos.first < 0 || n <= sub_pos.first || sub_pos.second < 0 || m <= sub_pos.second || box[sub_pos.first][sub_pos.second] != 0)
+			{
+				continue;
+			}
+
+			box[sub_pos.first][sub_pos.second] = 1;
+			days[sub_pos.first][sub_pos.second] = days[cur_pos.first][cur_pos.second] + 1;
+			tomato.push(sub_pos);
+		}
+	}
+
+	int max_days = 0;
+	bool all_ripe = true;
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < m; ++j)
+		{
+			if (box[i][j] == 0)
+			{
+				all_ripe = false;
+				break;
+			}
+			max_days = max(max_days, days[i][j]);
+		}
+	}
+
+	if (all_ripe)
+	{
+		cout << max_days << "\n";
+	}
+	else
+	{
+		cout << -1 << "\n";
+	}
+
+	for (i = 0; i < n; ++i)
+	{
 		delete[] box[i];
 		delete[] days[i];
 	}
 	delete[] box;
 	delete[] days;
+
 	return 0;
 }
