dc907492b090b3b8709c5004ce4acda02f91e6ff
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-08-09 23:29:20 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,49 +1,76 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-#define X first
-#define Y second
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-int board[55][55];
-int dx[4]{ 0,1,0,-1 }, dy[4]{ 1,0,-1,0 };
+	const int X[4] = { 1,-1,0,0 };
+	const int Y[4] = { 0,0,1,-1 };
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int t;
+	int t; // 테스트 케이스의 개수
 	cin >> t;
-	while (t--) {
-		int ans = 0;
-		int m, n, k;
-		cin >> m >> n >> k;
-		while (k--) {
-			int x, y;
+
+	while (t--)
+	{
+		int col; // 배추밭의 가로길이
+		int row; // 배추밭의 세로길이
+		int k; // 배추가 심어져 있는 위치의 개수
+		cin >> col >> row >> k;
+
+		vector<vector<bool>> field(row, vector<bool>(col, false)); // 배추밭
+
+		int x; // 배추의 위치(가로)
+		int y; // 배추의 위치(세로)
+		while (k--)
+		{
 			cin >> x >> y;
-			board[x][y] = 1;
+			field[y][x] = true;
 		}
-		queue<pair<int, int>> que;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (board[i][j] == 1) {
-					++ans;
-					que.push({ i,j });
-					board[i][j] = 0;
-					while (que.size()) {
-						for (int k = 0; k < 4; k++) {
-							int px = que.front().X + dx[k], py = que.front().Y + dy[k];
-							if (px < 0 || px >= m || py < 0 || py >= n) continue;
-							if (board[px][py] == 0) continue;
-							que.push({ px,py });
-							board[px][py] = 0;
+
+		int insect = 0;
+		queue<pair<int, int>>pos;
+
+		for (int r_ = 0; r_ < row; ++r_)
+		{
+			for (int c_ = 0; c_ < col; ++c_)
+			{
+				if (field[r_][c_])
+				{
+					++insect;
+
+					pos.push({ r_,c_ });
+					field[r_][c_] = false;
+
+					pair<int, int> cur;
+					pair<int, int> sub;
+					while (pos.size())
+					{
+						cur = pos.front();
+						pos.pop();
+
+						for (int d_ = 0; d_ < 4; ++d_)
+						{
+							sub = { cur.first + X[d_],cur.second + Y[d_] };
+							if (sub.first < 0 || row <= sub.first || sub.second < 0 || col <= sub.second || !field[sub.first][sub.second])
+							{
+								continue;
+							}
+
+							pos.push({ sub.first,sub.second });
+							field[sub.first][sub.second] = false;
 						}
-						que.pop();
 					}
 				}
 			}
 		}
-		cout << ans << "\n";
-		fill(&board[0][0], &board[54][55], 0);
+
+		cout << insect << '\n';
 	}
 
 	return 0;
