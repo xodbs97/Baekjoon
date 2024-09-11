94a24a092889dd302257debce85b9473dbe88b7f
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-09-12 03:17:06 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll POW(ll a, ll b, ll c) {
+	if (b == 1) return a % c;
+	ll val = POW(a, b / 2, c);
+	val = val * val % c;
+	if (b % 2 == 0) return val;
+	return val * a % c;
+}
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+	cout << POW(a, b, c);
+
+	return 0;
+}
