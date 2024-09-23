fc6392e7e859b5e0a888d607f0d587d93f59665e
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-09-23 23:46:03 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,25 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
 
 using namespace std;
 
-void func(int a, int b, int n) {
-	if (n == 1) {
-		cout << a << " " << b << "\n";
-		return;
-	}
-	func(a, 6 - a - b, n - 1);
-	cout << a << " " << b << "\n";
-	func(6 - a - b, b, n - 1);
-}
+stack<int> stk[3];
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+static void Hanoi(const int num, const int curpos, const int nxtpos);
 
-	int n;
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n; // 첫 번째 장대에 쌓인 원판의 개수
 	cin >> n;
-	cout << (1 << n) - 1 << "\n";
-	func(1, 3, n);
+
+	int k = 0; // 옮긴 횟수
+	for (int i = 1; i <= n; ++i)
+	{
+		k = k * 2 + 1;
+	}
+	cout << k << '\n';
+
+	for (int i = n; 0 < i; --i)
+	{
+		stk[0].push(i);
+	}
+
+	Hanoi(n, 1, 3);
 
 	return 0;
+}
+
+void Hanoi(const int num, const int curpos, const int nxtpos)
+{
+	if (num == 0)
+	{
+		return;
+	}
+
+	Hanoi(num - 1, curpos, 6 - curpos - nxtpos);
+
+	stk[curpos - 1].pop();
+	stk[nxtpos - 1].push(num);
+	cout << curpos << ' ' << nxtpos << '\n';
+
+	Hanoi(num - 1, 6 - curpos - nxtpos, nxtpos);
 }
