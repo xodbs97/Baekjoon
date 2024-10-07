3b738d63757e78eeea21d44b5fa2524ded241f6c
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-10-07 22:41:02 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,34 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int n, m;
-int arr[10];
-bool is_used[10];
+static int n;
+static int m;
+static bool used[8];
+static int order[8];
 
-void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++) cout << arr[i] << " ";
-		cout << "\n";
-		return;
-	}
+static void permutation(const int _len);
 
-	for (int i = 1; i <= n; i++) {
-		if (!is_used[i]) {
-			arr[k] = i;
-			is_used[i] = true;
-			func(k + 1);
-			is_used[i] = false;
-		}
-	}
-}
-
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	cin >> n >> m;
-	func(0);
+	permutation(0);
 
 	return 0;
+}
+
+void permutation(const int _len)
+{
+	if (_len == m)
+	{
+		for (int i = 0; i < m; ++i)
+		{
+			cout << order[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (!used[i])
+		{
+			used[i] = true;
+			order[_len] = i + 1;
+			permutation(_len + 1);
+			used[i] = false;
+		}
+	}
 }
