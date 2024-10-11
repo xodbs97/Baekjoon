3fafb588aade6ddcd8ffd1020a3d533082629204
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-10-11 22:52:58 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static int n;
+static int m;
+static int nums[8];
+static bool used[8];
+static vector<int> v;
+
+static void permutation(const int _idx, const int _len);
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	permutation(0, 0);
+
+	return 0;
+}
+
+void permutation(const int _idx, const int _len)
+{
+	if (_len == m)
+	{
+		for (int i = 0; i < m; ++i)
+		{
+			cout << nums[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = _idx; i < n; ++i)
+	{
+		if (!used[i])
+		{
+			used[i] = true;
+			nums[_len] = v[i];
+			permutation(i + 1, _len + 1);
+			used[i] = false;
+		}
+	}
+}
