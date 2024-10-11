38c77d49091685d90e6c7bcaf7deafeb209b4fbb
Kim Taeyoon <92427843+xodbs97@users.noreply.github.com>
2024-10-11 22:40:12 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,36 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-// 자연수 개수, 수열 길이
-int n, m;
-// 완성된 수열이 저장될 배열
-int arr[10];
+static int n;
+static int m;
+static int nums[8];
 
-// 현재 예비 번호, 현재 수열 길이
-void bt(int num, int k) {
-	// 수열이 완성됐다면
-	if (k == m) {
-		for (int i = 0; i < m; ++i) {
-			cout << arr[i] << " ";
+static void permutation(const int _idx, const int _num, const int _len);
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m;
+	permutation(0, 1, 0);
+
+	return 0;
+}
+
+void permutation(const int _idx, const int _num, const int _len)
+{
+	if (_len == m)
+	{
+		for (int i = 0; i < m; ++i)
+		{
+			cout << nums[i] << ' ';
 		}
-		cout << "\n";
+		cout << '\n';
 		return;
 	}
 
-	for (int i = num; i <= n; ++i) {
-		arr[k] = i;
-		bt(i, k + 1);
+	for (int i = _num; i <= n; ++i)
+	{
+		nums[_idx] = i;
+		permutation(_idx + 1, i, _len + 1);
 	}
-	return;
-}
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> n >> m;
-	bt(1, 0);
-
-	return 0;
 }
